cd046f4ef5f80576ba9ca749035742b04dee2127
Kucing Item Putih <poncoe4mac@gmail.com>
2017-10-27 22:28:39 +0700

    meow commit

---
+++ app/src/main/java/id/dreamcode/splashscreenprogressbar/MainActivity.java
@@ -0,0 +1,24 @@
+package id.dreamcode.splashscreenprogressbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+ 
+public class MainActivity extends Activity implements OnClickListener {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+		
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+		
+		}
+	}
